24d06e670ecf0112f5b67a9c76f307fac1bac239
Antac69 <angello_04_09@hotmail.com>
2023-07-18 21:38:45 -0500

    agregando funsiones al form

--- pages/new.jsx
+++ pages/new.jsx
@@ -1,34 +1,58 @@
 import React, { useState } from "react";
 
 const New = () => {
+  const [form, setForm] = useState({ title: "", plot: "" });
 
-    const[form,setForm]=useState({title:'',plot:''})
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit=e=>{
+    e.preventDefault()
+    postData(form)
+  }
+
+  const postData=async(form)=>{
+    try {
+        console.log(form)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
       <div className="contaniner">
         <h1 className="my-3">Agregar Movie</h1>
-        <form >
-            <input
-        className="form-control my-2"
-        type="text"
-        placeholder="Title"
-        autoComplete="off"
-        name="title"
-        value={form.title}
-        onChange={()=>} />
-            <input
-        className="form-control my-2"
-        type="text"
-        placeholder="Plot"
-        autoComplete="off"
-        name="plot"
-        value={form.plot}
-        onChange={} />
-        <button className="btn btn-primary w-100" type="submit">Agregar</button>
-        <Link href='/' className='btn btn-warning w-100'>
-        Volver...
-        </Link>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-control my-2"
+            type="text"
+            placeholder="Title"
+            autoComplete="off"
+            name="title"
+            value={form.title}
+            onChange={handleChange}
+          />
+          <input
+            className="form-control my-2"
+            type="text"
+            placeholder="Plot"
+            autoComplete="off"
+            name="plot"
+            value={form.plot}
+            onChange={handleChange}
+          />
+          <button className="btn btn-primary w-100" type="submit">
+            Agregar
+          </button>
+          <Link href="/" className="btn btn-warning w-100">
+            Volver...
+          </Link>
         </form>
       </div>
     </>
